dd80124b4844aa3ddffb21aa5e16045eada6e090
sansdoute1 <sandeepsingh287@hotmail.com>
2025-05-08 14:43:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ def home():
 
 import os
 
+@app.route('/place-bet', methods=['GET', 'POST'])
+def place_bet():
+    if request.method == 'POST':
+        challenge = request.form.get('challenge')
+        goal = request.form.get('goal')
+        timeline = request.form.get('timeline')
+        friends = request.form.get('friends')
+        amount = request.form.get('amount')
+        return render_template('place_bet.html', success=True, challenge=challenge)
+    return render_template('place_bet.html', success=False)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
