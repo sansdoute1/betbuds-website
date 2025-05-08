131559c27fc0ec2c553b79a629a016b75822888f
sansdoute1 <sandeepsingh287@hotmail.com>
2025-05-08 14:44:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -6,17 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-import os
-
 @app.route('/place-bet', methods=['GET', 'POST'])
 def place_bet():
     if request.method == 'POST':
+        # Get form inputs
         challenge = request.form.get('challenge')
         goal = request.form.get('goal')
         timeline = request.form.get('timeline')
         friends = request.form.get('friends')
         amount = request.form.get('amount')
+
+        # For now, just show a success message
         return render_template('place_bet.html', success=True, challenge=challenge)
+
     return render_template('place_bet.html', success=False)
 
 if __name__ == '__main__':
